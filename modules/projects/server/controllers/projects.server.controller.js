c17003d01d1c05d8b9427589494eb261497b8438
Syed Mohsin <syedm.90@gmail.com>
2017-02-15 10:44:44 -0800

    #124 org available on connection requests and add bidders modal in project view

--- modules/projects/server/controllers/projects.server.controller.js
+++ modules/projects/server/controllers/projects.server.controller.js
@@ -277,6 +277,10 @@ exports.projectByID = function (req, res, next, id) {
       User.populate(project.user, 
         {path: 'connections', 
           match: { _id: { $nin: bidder_ids }, roles: 'seller' },
+          populate: {
+            path: 'organization',
+            select: 'logoImageUrl name'
+          },
           select: "-password -salt -roles -connections -received_user_invites -sent_user_invites"}, function(err, connections) {
         if (err) {
           return next(err);
