d93f845d1e5413f4b8b3a2922cd0a3b06ab40d64
Syed Mohsin <syedm.90@gmail.com>
2017-02-28 21:28:59 -0800

    #155 allow user to transition from search page to catalog

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,12 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$state', 'Authentication',
+  function ($scope, $state, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.goToCatalog = function() {
+      $state.go('catalog');
+    };
   }
 ]);
