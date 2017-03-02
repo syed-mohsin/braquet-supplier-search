3a42862a896912669f07c7596729d9b941cfb36d
Syed Mohsin <syedm.90@gmail.com>
2017-03-01 23:40:55 -0800

    #124 allow user to access org details in profile views

--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -1,7 +1,13 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('users').controller('SettingsController', ['$scope', '$http', 'Authentication',
+  function ($scope, $http, Authentication) {
     $scope.user = Authentication.user;
+
+    // get user organization
+    $http.get('api/organizations/' + $scope.user.organization)
+      .success(function(org) {
+        $scope.user.organization = org;
+      });
   }
 ]);
