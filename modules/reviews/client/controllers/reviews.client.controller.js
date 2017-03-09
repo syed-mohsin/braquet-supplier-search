39e6e5f619fd9975a46fead7200891100f4bf71c
Syed Mohsin <syedm.90@gmail.com>
2017-03-08 22:30:53 -0800

    #177 allow anonymous reviews

--- modules/reviews/client/controllers/reviews.client.controller.js
+++ modules/reviews/client/controllers/reviews.client.controller.js
@@ -5,34 +5,6 @@ angular.module('reviews').controller('ReviewsController', ['$scope', '$statePara
   function ($scope, $stateParams, $location, Authentication, Reviews) {
     $scope.authentication = Authentication;
 
-    // Create new Review
-    $scope.create = function (isValid) {
-      $scope.error = null;
-
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'reviewForm');
-
-        return false;
-      }
-
-      // Create new Review object
-      var review = new Reviews({
-        title: this.title,
-        content: this.content
-      });
-
-      // Redirect after save
-      review.$save(function (response) {
-        $location.path('reviews/' + response._id);
-
-        // Clear form fields
-        $scope.title = '';
-        $scope.content = '';
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
-
     // Remove existing Review
     $scope.remove = function (review) {
       if (review) {
