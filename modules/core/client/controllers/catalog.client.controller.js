42f6f3b2097c4b251f672b4e91a79c97ddea77e0
Syed Mohsin <syedm.90@gmail.com>
2017-03-11 23:25:07 -0800

    #195 add brand checkbox functionality and refactor

--- modules/core/client/controllers/catalog.client.controller.js
+++ modules/core/client/controllers/catalog.client.controller.js
@@ -26,7 +26,6 @@ angular.module('core').controller('CatalogController', ['$scope', '$filter', '$h
       }
     })
     .success(function(resp) {
-      $scope.orgsAll = resp.orgs;
       $scope.orgs = resp.orgs;
       $scope.buildPager(resp.count);
 
@@ -35,71 +34,28 @@ angular.module('core').controller('CatalogController', ['$scope', '$filter', '$h
     });
 
     $scope.updateFilter = function() {
-      var temp = $scope.orgs;
-      $scope.orgs = [];
+      var man = '';
+      var pow = '';
 
       // find all checked boxes for wattage
-      var checked = [];
       for (var key in $scope.wattCheckboxes) {
         if ($scope.wattCheckboxes[key]) {
-          checked.push(key);
+          pow += key + '|';
         }
       }
 
       // find all checked manufacturers
-      $scope.checkedManufacturers = [];
       for (key in $scope.orgCheckboxes) {
         if ($scope.orgCheckboxes[key]) {
-          $scope.checkedManufacturers.push(key);
+          man += key + '|';
         }
       }
 
-      $scope.calculateItemsToDisplay(checked);
-      $scope.figureOutItemsToDisplay();
+      $scope.query.man = man;
+      $scope.query.pow = pow;
+      $state.go('catalog', $scope.query);
     };
 
-    $scope.calculateItemsToDisplay = function(checked) {
-      $scope.orgs = [];
-      if (!checked.length) {
-        $scope.orgs = $scope.orgsAll;
-      } else {
-        // filter wattage range first
-        $scope.orgs = $scope.orgsAll.filter(function(org) {
-          return org.panel_models.some(function(panel) {
-            var finalCheck = false;
-            var wattage = panel.stcPower;
-            var manufacturer = panel.manufacturer;
-            checked.forEach(function(check) {
-              if (check === '  0 - 100 Watts') finalCheck = finalCheck || wattage <= 100;
-              else if (check === '101 - 200 Watts') finalCheck = finalCheck || (wattage > 100 && wattage <= 200);
-              else if (check === '201 - 300 Watts') finalCheck = finalCheck || (wattage > 200 && wattage <= 300);
-              else if (check === '301 - 400 Watts') finalCheck = finalCheck || (wattage > 300 && wattage <= 400);
-              else if (check === '401 - 500 Watts') finalCheck = finalCheck || (wattage > 400 && wattage <= 500);
-            });
-
-            return finalCheck;
-          });
-        });
-      }
-
-      if (!$scope.checkedManufacturers.length) {
-        return;
-      }
-      else {
-        // filter by manufacturers
-        $scope.orgs = $scope.orgs.filter(function(org) {
-          return org.panel_models.some(function(panel) {
-            var finalCheck = false;
-            var manufacturer = panel.manufacturer;
-            $scope.checkedManufacturers.forEach(function(check) {
-              finalCheck = finalCheck || (manufacturer === check);
-            });
-
-            return finalCheck;
-          });
-        });
-      }
-    };
 
     $scope.toggleFilter = function() {
       $scope.hiddenFilterClass = $scope.hiddenFilterClass ? '' : 'hidden-xs';
@@ -125,27 +81,17 @@ angular.module('core').controller('CatalogController', ['$scope', '$filter', '$h
         .success(function(data) {
           $scope.manufacturers = data;
           $scope.orgCheckboxes = {};
+          var queryCheckedBoxes = $state.params.man.split('|');
           data.forEach(function(manufacturer) {
-            $scope.orgCheckboxes[manufacturer] = false;
+            $scope.orgCheckboxes[manufacturer] = queryCheckedBoxes.indexOf(manufacturer) !== -1 ? true : false;
           });
         });
     };
 
     $scope.buildPager = function (count) {
-      $scope.pagedItems = [];
       $scope.itemsPerPage = 15;
       $scope.totalCount = count;
       $scope.currentPage = $stateParams.page;
-    };
-
-    $scope.figureOutItemsToDisplay = function () {
-      $scope.filteredItems = $filter('filter')($scope.orgs, {
-        $: $scope.search
-      });
-      $scope.filterLength = $scope.filteredItems.length;
-      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
-      var end = begin + $scope.itemsPerPage;
-      $scope.pagedItems = $scope.filteredItems.slice(begin, end);
     };
 
     $scope.pageChanged = function () {
