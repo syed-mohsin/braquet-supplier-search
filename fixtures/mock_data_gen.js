2262027c0a490770d913f64a3aefa7b6433ee579
Syed Mohsin <syedm.90@gmail.com>
2017-03-13 11:06:06 -0700

    #196 fix mongoose require issue

--- fixtures/mock_data_gen.js
+++ fixtures/mock_data_gen.js
@@ -2,7 +2,6 @@
 
 var mongoose = require('mongoose');
 var dbUrl = 'mongodb://localhost/mean-dev';
-var Organization;
 
 mongoose.connect(dbUrl, function(err) {
   if (!err) {
@@ -12,18 +11,16 @@ mongoose.connect(dbUrl, function(err) {
 
 // register relevant models
 require('../modules/organizations/server/models/organization.server.model');
-console.log(mongoose.models);
 require('../modules/panels/server/models/panelmodel.server.model');
-console.log(mongoose.models);
 require('../modules/users/server/models/user.server.model');
-console.log(mongoose.models);
 require('../modules/reviews/server/models/review.server.model');
-console.log(mongoose.models);
 
 
-  // Organization = mongoose.model('Organization');
-  // Organization.find({}, function(err, orgs) {
-  //   console.log('hello');
-  //   if (err) console.log(err);
-  //   else console.log(orgs.length);
-  // });
+
+var Organization = mongoose.model('Organization');
+
+Organization.find()
+  .exec()
+  .then(function(orgs) {
+    console.log(orgs.length);
+  });
