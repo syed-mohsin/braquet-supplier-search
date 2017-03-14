278b309afcdd7f4b852f7a5542dfa222413e4449
Syed Mohsin <syedm.90@gmail.com>
2017-03-13 19:19:34 -0700

    #195 move env variables into source env.sh file

--- config/env/production.js
+++ config/env/production.js
@@ -27,7 +27,7 @@ module.exports = {
         directoryPath: process.env.LOG_DIR_PATH || process.cwd(),
         fileName: process.env.LOG_FILE || 'access.log',
         rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-          active: process.env.LOG_ROTATING_ACTIVE === 'true' ? true : false, // activate to use rotating logs 
+          active: process.env.LOG_ROTATING_ACTIVE === 'true' ? true : false, // activate to use rotating logs
           fileName: process.env.LOG_ROTATING_FILE || 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
           frequency: process.env.LOG_ROTATING_FREQUENCY || 'daily',
           verbose: process.env.LOG_ROTATING_VERBOSE === 'true' ? true : false
@@ -101,3 +101,13 @@ module.exports = {
     }
   }
 };
+
+if (process.env.NODE_ENV !== 'development') {
+  module.exports.db = {
+    uri: process.env.MONGOLAB_URI,
+    options: {
+      user: process.env.MONGOLAB_USER,
+      pass: process.env.MONGOLAB_PASS
+    }
+  };
+}
