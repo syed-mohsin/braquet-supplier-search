ff711837f83ed8f2020a186473aa9b611e17c8e2
Syed Mohsin <syedm.90@gmail.com>
2017-03-15 14:11:43 -0700

    #224 change organization menu item to single click to see list of organizations

--- modules/organizations/client/config/organizations.client.config.js
+++ modules/organizations/client/config/organizations.client.config.js
@@ -6,23 +6,8 @@ angular.module('organizations').run(['Menus',
     // Add the projects dropdown item
     Menus.addMenuItem('topbar', {
       title: 'Company Directory',
-      state: 'organizations',
-      type: 'dropdown',
-      roles: ['user', 'seller']
-    });
-
-    // Add the dropdown list item
-    Menus.addSubMenuItem('topbar', 'organizations', {
-      title: 'Company Directory',
       state: 'organizations.list',
-      roles: ['user', 'seller']
-    });
-
-    // Add the dropdown create item
-    Menus.addSubMenuItem('topbar', 'organizations', {
-      title: 'Add Organizations',
-      state: 'organizations.create',
-      roles: ['admin']
+      roles: ['user', 'seller', 'admin']
     });
   }
 ]);
