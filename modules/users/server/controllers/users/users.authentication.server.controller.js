5e00745e9887fa1c422cea047a21fd0cb2547c75
dnajafi <dbnajafi@gmail.com>
2017-03-16 15:08:06 -0700

    #200 place all function in user.verify-email into async waterfall

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -206,7 +206,9 @@ exports.signup = function (req, res) {
       });
     },
     function(emailHTML, done) {
-      var mailList = 'syedm.90@gmail.com, takayuki.koizumi@gmail.com, dbnajafi@gmail.com';
+      // var mailList = 'syedm.90@gmail.com, takayuki.koizumi@gmail.com, dbnajafi@gmail.com';
+
+      var mailList = 'dbnajafi@gmail.com';
 
       var mailOptions = {
         to: mailList,
--- modules/users/server/controllers/users/users.verify-email.server.controller.js
+++ modules/users/server/controllers/users/users.verify-email.server.controller.js
@@ -19,89 +19,100 @@ var smtpTransport = nodemailer.createTransport(config.mailer.options);
  * Validate token for email confirmation
  */
 exports.validateEmail = function (req, res) {
-  User.findOne({
-    inviteToken: req.params.token,
-  }, function (err, user) {
-    if (!user) {
-      return res.redirect('/forbidden');
-    }
 
-    // verify user has confirmed email
-    user.emailVerified = true;
+  async.waterfall([
+    function(done){
+      User.findOne({ inviteToken: req.params.token }, function(err, user){
+        if (!user) {
+          return res.redirect('/forbidden');
+        }
+        console.log(user);
 
-    user.save(function(err) {
-      if (err) {
-        return res.redirect('/forbidden');
-      } else {
-        // successfully verified email
-        req.login(user, function (err) {
+        done(err, user);
+      });
+    },
+    function(user, done){
+      console.log('*******111111**********');
+      console.log(user);
+      console.log('*******1.5**********');
+      // verify user has confirmed email
+      user.emailVerified = true;
+
+      Review.update(
+        {
+          user: user._id,
+          verified: false
+        },
+        { 
+          $set: { verified: true }
+        }, 
+        function(err, reviews) {
           if (err) {
+            console.log('*******ERROR**********');
             return res.redirect('/forbidden');
-          } else {
-
-            async.waterfall([
-              function(done) {
-                res.render('modules/users/server/templates/notify-email', {
-                  name: user.displayName,
-                  email: user.email
-                }, function(err, emailHTML) {
-                  done(err, emailHTML);
-                });
-              },
-              // send notify email to Braquet admin using service
-              function(emailHTML, done) {
-                var mailList = 'syedm.90@gmail.com, takayuki.koizumi@gmail.com, dbnajafi@gmail.com';
-
-                var mailOptions = {
-                  to: mailList,
-                  from: config.mailer.from,
-                  subject: 'Braquet - Notification of User Confirmation',
-                  html: emailHTML
-                };
-
-                smtpTransport.sendMail(mailOptions, function (err) {
-                  if (err) {
-                    return res.status(400).send({
-                      message: 'Failure sending notification email'
-                    });
-                  }
-                });
-              }
-            ], function(err) {
-              if(err) {
-                return res.status(400).send({
-                  message: errorHandler.getErrorMessage(err)
-                });
-              }
-            });
-            res.redirect('/');
           }
+          console.log('*******1.6666**********');
+          done(err, user);
+        });
+    },
+    function(user, done) {
+      console.log('*******2222222**********');
+      user.save(function(err) {
+        if(err) {
+          return res.redirect('/forbidden');
+        } else {
+          // successfully verified email
+          done(err, user);
+        }
+      });
+    },
+    function(user, done) {
+      console.log('*******3333333**********');
+      // successfully verified email
+      req.login(user, function (err) {
+        if(err) {
+          return res.redirect('/forbidden');
+        } else {
+          res.render('modules/users/server/templates/notify-email', {
+            name: user.displayName,
+            email: user.email
+          }, function(err, emailHTML) {
+            done(err, emailHTML, user);
+          });
+        }
+      });
+    },
+    // send notify email to Braquet admin using service
+    function(emailHTML, user, done) {
+      console.log('*******444444**********');
+
+      // var mailList = 'syedm.90@gmail.com, takayuki.koizumi@gmail.com, dbnajafi@gmail.com';
+
+      var mailList = 'dbnajafi@gmail.com';
+
+      var mailOptions = {
+        to: mailList,
+        from: config.mailer.from,
+        subject: 'Braquet - Notification of User Confirmation',
+        html: emailHTML
+      };
+
+      smtpTransport.sendMail(mailOptions, function (err) {
+        if (err) {
+          return res.status(400).send({
+            message: 'Failure sending notification email'
+          });
+        }
+        return res.redirect('/');
+      });
+    }],
+    function(err) {
+      if(err) {
+        console.log('*******HOLY SHIT**********');
+        return res.status(400).send({
+          message: errorHandler.getErrorMessage(err)
         });
       }
-
-      // set all user's reviews to verified
-      Review.update({
-        user: user._id,
-        verified: false
-      }, { $set: { verified: true } }, function(err, reviews) {
-        if (err) return res.redirect('/forbidden');
-
-        // finally save and log in user
-        user.save(function(err) {
-          if (err) {
-            return res.redirect('/forbidden');
-          } else {
-            // successfully verified email
-            req.login(user, function (err) {
-              if (err) {
-                return res.redirect('/forbidden');
-              } else {
-                res.redirect('/');
-              }
-            });
-          }
-        });
-      });
-    });
-  });
+    }
+  );
 };
