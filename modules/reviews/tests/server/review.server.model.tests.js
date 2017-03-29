8e8931cc14a1053c6ad366f8f1fb9ef5022acf53
dnajafi <dbnajafi@gmail.com>
2017-03-29 10:46:26 -0700

    #241 finish grunt testing without using nvm

--- modules/reviews/tests/server/review.server.model.tests.js
+++ modules/reviews/tests/server/review.server.model.tests.js
@@ -53,7 +53,8 @@ describe('Review Model Unit Tests:', function () {
   describe('Method Save', function () {
     it('should be able to save without problems', function (done) {
       this.timeout(10000);
-      return review.save(function (err) {
+      review.save(function (err) {
+        console.log('logging error:', err);
         should.not.exist(err);
         done();
       });
@@ -61,8 +62,7 @@ describe('Review Model Unit Tests:', function () {
 
     it('should be able to show an error when try to save without title', function (done) {
       review.title = '';
-
-      return review.save(function (err) {
+      review.save(function (err) {
         should.exist(err);
         done();
       });
