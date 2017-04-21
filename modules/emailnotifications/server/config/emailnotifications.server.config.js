8783a5d2c81eeae13755f19f6b04f41c6dfb30ed
Syed Mohsin <syedm.90@gmail.com>
2017-04-20 20:09:23 -0700

    #329 add job scheduling mechanism

--- modules/emailnotifications/server/config/emailnotifications.server.config.js
+++ modules/emailnotifications/server/config/emailnotifications.server.config.js
@@ -4,11 +4,21 @@
  * Module dependencies
  */
 var path = require('path'),
-  config = require(path.resolve('./config/config'));
+  config = require(path.resolve('./config/config')),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User'),
+  EmailNotification = mongoose.model('EmailNotification'),
+  schedule = require('node-schedule');
 
 /**
  * Module init function.
  */
 module.exports = function (app, db) {
+  // initialize scheduled emailing
+  var rule = new schedule.RecurrenceRule();
+  rule.minute = 9;
 
+  var j = schedule.scheduleJob(rule, function(){
+    console.log('The answer to life, the universe, and everything!');
+  });
 };
