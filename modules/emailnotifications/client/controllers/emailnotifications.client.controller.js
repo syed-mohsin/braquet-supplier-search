30d52d724e645183152245e0c4f3a8c38ceab6f8
Syed Mohsin <syedm.90@gmail.com>
2017-04-25 15:17:53 -0700

    #329 add follow button for logged-in organization view

--- modules/emailnotifications/client/controllers/emailnotifications.client.controller.js
+++ modules/emailnotifications/client/controllers/emailnotifications.client.controller.js
@@ -5,51 +5,6 @@ angular.module('emailNotifications').controller('EmailNotificationsController', 
   function ($scope, $stateParams, $location, Authentication, EmailNotifications) {
     $scope.authentication = Authentication;
 
-    // Create new EmailNotification
-    $scope.create = function (isValid) {
-      $scope.error = null;
-
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'emailNotificationForm');
-
-        return false;
-      }
-
-      // Create new EmailNotification object
-      var emailNotification = new EmailNotifications({
-        title: this.title,
-        content: this.content
-      });
-
-      // Redirect after save
-      emailNotification.$save(function (response) {
-        $location.path('emailNotifications/' + response._id);
-
-        // Clear form fields
-        $scope.title = '';
-        $scope.content = '';
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
-
-    // Remove existing EmailNotification
-    $scope.remove = function (emailNotification) {
-      if (emailNotification) {
-        emailNotification.$remove();
-
-        for (var i in $scope.emailNotifications) {
-          if ($scope.emailNotifications[i] === emailNotification) {
-            $scope.emailNotifications.splice(i, 1);
-          }
-        }
-      } else {
-        $scope.emailNotification.$remove(function () {
-          $location.path('emailNotifications');
-        });
-      }
-    };
-
     // Update existing EmailNotification
     $scope.update = function (isValid) {
       $scope.error = null;
