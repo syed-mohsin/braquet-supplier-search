933f083b5dbd917da25502826734150a74e06cf7
Syed Mohsin <syedm.90@gmail.com>
2017-04-26 19:13:58 -0700

    #329 refactor email notification code and move into service

--- modules/emailnotifications/server/config/emailnotifications.server.config.js
+++ modules/emailnotifications/server/config/emailnotifications.server.config.js
@@ -5,12 +5,7 @@
  */
 var path = require('path'),
   config = require(path.resolve('./config/config')),
-  mongoose = require('mongoose'),
-  User = mongoose.model('User'),
-  Organization = mongoose.model('Organization'),
-  OrganizationService = require('../../../organizations/server/services/organizations.server.service'),
-  EmailNotification = mongoose.model('EmailNotification'),
-  mailgun = require('mailgun-js')({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN }),
+  EmailNotificationService = require('../services/emailnotifications.server.service'),
   schedule = require('node-schedule');
 
 /**
@@ -19,63 +14,17 @@ var path = require('path'),
 module.exports = function (app, db) {
   // initialize scheduled emailing
   var rule = new schedule.RecurrenceRule();
-  var ONE_DAY = 24*60*60*1000;
-  var dateCheck = new Date(Date.now() - ONE_DAY);
   rule.minute = 44;
 
-  var data = {
-    from: 'Braquet <no-reply@braquet.io>',
-    to: process.env.NOTIFICATION_TEST_EMAIL,
-    subject: 'Test Update'
-  };
+  var job = schedule.scheduleJob(rule, function(){
 
-  User.findOne({ email: process.env.NOTIFICATION_TEST_EMAIL }).exec()
-  .then(function(user) {
-    return EmailNotification.findOne({ user: user._id }).exec();
-  })
-  .then(function(emailNotification) {
-    return Organization.find({
-      _id: { $in: emailNotification.followingOrganizations },
-      updated: { $gt: dateCheck }
-    }).populate('priceReviews').lean().exec();
-  })
-  .then(function(orgs) {
-    orgs = OrganizationService.extractBrands(orgs);
-    orgs = orgs.filter(function(org) {
-      return isFinite(org.brands_avg_min);
-    });
-    orgs = orgs.map(function(org) {
-      org.id = org._id.toString();
-      return org;
-    });
-
-    return new Promise(function(resolve, reject) {
-
-      app.render('modules/emailnotifications/server/templates/user-update', {
-        organizations: orgs
-      }, function(err, emailHTML) {
-        if(err) {
-          reject(err);
-        }
-        resolve(emailHTML);
-      });
-    });
-  })
-  .then(function(emailHTML) {
-    data.html = emailHTML;
-    mailgun.messages().send(data, function (err, body) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('body', body);
-      }
-    });
-  })
-  .catch(function(err) {
-    console.log('failed to fetch user', err);
   });
 
-  var j = schedule.scheduleJob(rule, function(){
-
+  EmailNotificationService.sendEmailToUser(app)
+  .then(function(body) {
+    console.log(body);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
 };
