4d0b26f285b77b3dd42b58966432b9db20fe2ef6
Syed Mohsin <syedm.90@gmail.com>
2017-05-01 18:11:48 -0700

    #332 remove user role from signup

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -63,11 +63,13 @@ exports.signup = function (req, res) {
   ;
 
   // Define user role, seller if user_role = 1 (user_role = 0 defaults to user a.k.a buyer)
-  if (req.body.user_role === '1')
-    user.roles = ['seller'];
-  else {
-    user.roles = ['user'];
-  }
+  // if (req.body.user_role === '1')
+  //   user.roles = ['seller'];
+  // else {
+  //   user.roles = ['user'];
+  // }
+
+  user.roles = ['user'];
 
   // check if user was invited and connect upon signup
   var err = '';
