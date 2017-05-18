bc334a980c9d37bd56ce2518e06b790e5e611734
Syed Mohsin <syedm.90@gmail.com>
2017-05-18 14:35:39 -0400

    #359 add pagination and improved navigation to public org view

--- modules/organizations/client/controllers/view-public-organization.client.controller.js
+++ modules/organizations/client/controllers/view-public-organization.client.controller.js
@@ -2,38 +2,68 @@
 
 // Organizations controller
 
-angular.module('organizations').controller('PublicViewOrganizationController', ['$rootScope', '$scope', '$state', '$stateParams', '$http', '$location', '$timeout', '$interval', '$filter', '$window', 'Authentication', 'Socket',
-  function ($rootScope, $scope, $state, $stateParams, $http, $location, $timeout, $interval, $filter, $window, Authentication, Socket) {
+angular.module('organizations').controller('PublicViewOrganizationController', ['$rootScope', '$scope', '$state', '$stateParams', '$http', '$location', '$timeout', '$interval', '$filter', '$window', 'Authentication', 'Socket', 'Pagination',
+  function ($rootScope, $scope, $state, $stateParams, $http, $location, $timeout, $interval, $filter, $window, Authentication, Socket, Pagination) {
     $scope.authentication = Authentication;
     $scope.user = Authentication.user;
 
+    $scope.showView = function(viewType, itemsArray, page) {
+      $scope.viewType = viewType;
+
+      $scope.pageSettings = Pagination.buildPage(itemsArray, page);
+    };
+
+    $scope.shouldShowType = function(viewType) {
+      return $scope.viewType === viewType;
+    };
+
+    $scope.changeTab = function(viewType) {
+      $stateParams.view = viewType;
+      $stateParams.page = undefined;
+
+      $state.go('organizations.view-public', $stateParams);
+    };
+
     $scope.initializePageNavBar = function() {
-      // tab viewing booleans
-      $scope.shouldShowReviews = false;
-      $scope.shouldShowPrices = true;
-      $scope.shouldShowProducts = false;
+      var page = $stateParams.page;
+
+      var viewTypes = {
+        'reviews': $scope.organization.reviews,
+        'prices': $scope.organization.priceReviews,
+        'products': $scope.organization.panel_models
+      };
+
+      // by default or by invalid param, set default to show Prices
+      if (!($stateParams.view in viewTypes)) {
+        $scope.showView('prices', $scope.organization.priceReviews, page);
+        return;
+      }
+
+      // find valid view
+      Object.keys(viewTypes).forEach(function(viewType) {
+        if (viewType === $stateParams.view) {
+          $scope.showView(viewType, viewTypes[viewType], page);
+        }
+      });
     };
 
-    $scope.showReviews = function() {
-      $scope. shouldShowReviews = true;
-      $scope.shouldShowPrices = false;
-      $scope.shouldShowProducts = false;
-    };
+    $scope.pageChanged = function() {
+      $stateParams.page = $scope.pageSettings.currentPage;
+      $stateParams.view = null;
 
-    $scope.showPrices = function() {
-      $scope. shouldShowReviews = false;
-      $scope.shouldShowPrices = true;
-      $scope.shouldShowProducts = false;
-    };
+      var viewTypes = ['reviews', 'prices', 'products'];
 
-    $scope.showProducts = function() {
-      $scope. shouldShowReviews = false;
-      $scope.shouldShowPrices = false;
-      $scope.shouldShowProducts = true;
-    };
+      // find valid view
+      viewTypes.forEach(function(viewType) {
+        if (viewType === $scope.viewType) {
+          $stateParams.view = $scope.viewType;
+        }
+      });
 
-    // initialize tabs
-    $scope.initializePageNavBar();
+      if (!$stateParams.view) $stateParams.view = 'prices';
+
+      $state.go('organizations.view-public', $stateParams);
+    };
 
     $scope.findOne = function() {
       if (Authentication.user) {
@@ -49,6 +79,9 @@ angular.module('organizations').controller('PublicViewOrganizationController', [
           var defaultDescr = 'See Reviews, Quotes, and Products for Suppliers. ';
           $rootScope.pageTitle = $scope.organization.companyName + ' | Braquet';
           $rootScope.description = defaultDescr + $scope.organization.about;
+
+          // initialize tabs
+          $scope.initializePageNavBar();
         })
         .catch(function(resp) {
           console.log('error finding org', resp.data);
